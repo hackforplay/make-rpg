1e8597001a0a8b2bb63599a6d3a26034fd7964bd
teramotodaiki <i101323@gmail.com>
2017-09-13 13:40:54 +0900

    game, maps, update in English

---
+++ src/i18n/en_US/game.js
@@ -0,0 +1,18 @@
+import 'hackforplay/core';
+// import 'mod/3d/core';
+
+async function gameFunc() {
+
+	Hack.changeMap('map1'); // Load map1 
+
+	self.player = new Player(); // Create a player 
+	player.mod(('▼ スキン', _kきし)); // Costume of player
+	player.locate(3, 5); // Location of player when start
+	/*+ スキル */
+
+
+
+	/*+ モンスター アイテム せっち システム */
+}
+
+export default gameFunc;
