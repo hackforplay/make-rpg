d24d1476c5a73090e8bf4e1c92d6b45bdb573f20
kassy_kz <kashimoto.kazutoshi@gmail.com>
2017-11-25 18:46:14 +0900

    敵の体力を全て1に by kassy

--- src/stages/3/game.js
+++ src/stages/3/game.js
@@ -5,6 +5,7 @@ import * as sequence from 'sequence';
  * デバッグ中につき魔道書は最初から表示されています
  */
 var mDragonScore = 20; 
+var mDragonHp = 1;
 var flagGem1 = false;
 var flagGem2 = false;
 var flagGem3 = false;
@@ -125,13 +126,13 @@ function resetMap() {
 	const itemGem1 = new RPGObject();
 	flagGem1 = false;	
 	itemGem1.mod(('▼ スキン', _tつぼ));
-	itemGem1.hp = 50;
+	itemGem1.hp = 1;
 	itemGem1.locate(4, 3, 'map1');
 	itemGem1.tl.moveBy(0, 96, 60).moveBy(0, -96, 60).loop();
 	itemGem1.on(('▼ イベント', 'たおれたとき'), () => {
 		flagGem1 = true;
 		if (flagGem2 && flagGem3) {
-			itemDragon.hp = 999;
+			itemDragon.hp = mDragonHp;
 			itemBarrier.visible = false;
 		}
 	});
@@ -140,13 +141,13 @@ function resetMap() {
 	const itemGem2 = new RPGObject();
 	flagGem2 = false;	
 	itemGem2.mod(('▼ スキン', _tつぼ));
-	itemGem2.hp = 50;
+	itemGem2.hp = 1;
 	itemGem2.locate(10, 6, 'map1');
 	itemGem2.tl.moveBy(0, -96, 60).moveBy(0, 96, 60).loop();
 	itemGem2.on(('▼ イベント', 'たおれたとき'), () => {
 		flagGem2 = true;
 		if (flagGem1 && flagGem3) {
-			itemDragon.hp = 999;
+			itemDragon.hp = mDragonHp;
 			itemBarrier.visible = false;
 		}
 	});
@@ -155,13 +156,13 @@ function resetMap() {
 	const itemGem3 = new RPGObject();
 	flagGem3 = false;
 	itemGem3.mod(('▼ スキン', _tつぼ));
-	itemGem3.hp = 50;
+	itemGem3.hp = 1;
 	itemGem3.locate(8, 2, 'map1');
 	itemGem3.tl.moveBy(-64, 0, 60).moveBy(64, 0, 60).loop();
 	itemGem3.on(('▼ イベント', 'たおれたとき'), () => {
 		flagGem3 = true;
 		if (flagGem1 && flagGem2) {
-			itemDragon.hp = 999;
+			itemDragon.hp = mDragonHp;
 			itemBarrier.visible = false;
 		}
 	});
