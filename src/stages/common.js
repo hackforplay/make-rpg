39b3028b9f48dd6497d314905a89f14bb35ab553
teramotodaiki <i101323@gmail.com>
2017-11-25 17:39:42 +0900

    score labels

--- src/stages/common.js
+++ src/stages/common.js
@@ -1,9 +1,9 @@
-/* global Hack, Sprite, game */
+/* global Hack, enchant, game */
 // 全てのステージに共通する処理
 
 const common = () => {
 	// 呪文詠唱を止めるボタン
-	const stopButton = new Sprite(80, 20);
+	const stopButton = new enchant.Sprite(80, 20);
 	stopButton.image = game.assets['resources/stop_button'];
 	stopButton.moveTo(0, 0);
 	stopButton.ontouchstart = () => {
@@ -12,13 +12,25 @@ const common = () => {
 	Hack.menuGroup.addChild(stopButton);
 	
 	// ゲームリセットボタン
-	const resetButton = new Sprite(80, 20);
+	const resetButton = new enchant.Sprite(80, 20);
 	resetButton.image = game.assets['resources/reset_button'];
 	resetButton.moveTo(0, 24);
 	resetButton.ontouchstart = () => {
 		Hack.dispatchEvent(new Event('reset'));
 	};
 	Hack.menuGroup.addChild(resetButton);
+
+	// スコアの表示位置変更
+	Hack.scoreLabel.moveTo(300, 8);
+	Hack.scoreLabel.backgroundColor = 'rgba(0, 0, 0, 0.5)';
+
+	// 階層ラベル (同じマップになんども enter することを想定している)
+	Hack.floorLabel = new enchant.ui.ScoreLabel(120, 8);
+	Hack.scoreLabel.backgroundColor = 'rgba(0, 0, 0, 0.5)';
+	Hack.floorLabel.score = 1;
+	Hack.floorLabel.label = 'FLOOR:';
+	Hack.menuGroup.addChild(Hack.floorLabel);
+
 };
 
 export default common;
