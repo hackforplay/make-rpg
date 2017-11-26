8e510fe9b0b138ec8463088dde6e4a2c3d69ebd9
teramotodaiki <i101323@gmail.com>
2017-11-26 15:50:39 +0900

    コードを空に

--- src/stages/3/code.js
+++ src/stages/3/code.js
@@ -1,16 +1,17 @@
-turnLeft();
-walk(3);
-repeat(2);
 
-locate(1, 5);
 
-headRight();
-headDown();
-headLeft();
-headUp();
 
-turnRight();
-dash();
-attack(2);
 
-repeat(1);
+
+
+
+
+
+
+
+
+
+
+
+
+
