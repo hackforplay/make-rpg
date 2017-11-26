e5586365e3859707607f5c5374dc15a475416f2f
teramotodaiki <i101323@gmail.com>
2017-11-26 09:26:57 +0900

    formatting

--- src/stages/2/game.js
+++ src/stages/2/game.js
@@ -7,38 +7,38 @@ import * as sequence from 'sequence';
 var mTresureBoxScore = 5;
 
 async function gameFunc() {
-  resetMap();
+	resetMap();
 
-  const player = (self.player = new Player()); // プレイヤーをつくる
-  player.locate(1, 1); // はじめの位置
-  player.on(('▼ イベント', 'こうげきするとき'), event => {
-    const 使い手 = event.target;
-    const ビーム = new RPGObject();
-    ビーム.mod(('▼ スキン', _bビーム));
-    ビーム.onふれはじめた = event => {
-      if (event.hit !== 使い手) {
-        Hack.Attack(event.mapX, event.mapY, 使い手.atk);
-        ビーム.destroy();
-      }
-    };
-    使い手.shoot(ビーム, 使い手.forward, 10);
-  });
-  /*+ スキル */
+	const player = (self.player = new Player()); // プレイヤーをつくる
+	player.locate(1, 1); // はじめの位置
+	player.on(('▼ イベント', 'こうげきするとき'), event => {
+		const 使い手 = event.target;
+		const ビーム = new RPGObject();
+		ビーム.mod(('▼ スキン', _bビーム));
+		ビーム.onふれはじめた = event => {
+			if (event.hit !== 使い手) {
+				Hack.Attack(event.mapX, event.mapY, 使い手.atk);
+				ビーム.destroy();
+			}
+		};
+		使い手.shoot(ビーム, 使い手.forward, 10);
+	});
+	/*+ スキル */
 
-  // さいしょの向きをかえる
-  player.turn(1);
+	// さいしょの向きをかえる
+	player.turn(1);
 
-  // 詠唱待ち時間設定
-  window.WAIT_TIME = 3000;
+	// 詠唱待ち時間設定
+	window.WAIT_TIME = 3000;
 
-  // ゲーム時間設定
-  window.TIME_LIMIT = 180 * 1000;
+	// ゲーム時間設定
+	window.TIME_LIMIT = 180 * 1000;
 
-  // タイマー開始
-  Hack.startTimer();
+	// タイマー開始
+	Hack.startTimer();
 
-  // 魔道書のコードをひらく
-  feeles.openCode('stages/2/code.js');
+	// 魔道書のコードをひらく
+	feeles.openCode('stages/2/code.js');
 
 	for (const key of Object.keys(sequence)) {
 		if (key !== 'resetQueue') {
@@ -49,7 +49,7 @@ async function gameFunc() {
 }
 
 function resetMap() {
-  const map1 = Hack.createMap(`
+	const map1 = Hack.createMap(`
 		10|10|10|10|10|10|10|10|10|10|10|10|10|10|10|10|10|10|10|10|10|10|10|
 		10|03 03 03 03 03 03 03 03 03 03 03 03 03 03 03 03 03 03 03 03 03 10|
 		10|10|10|10|10|10|10|10|10|10|10|10|10|10|10|10|10|10|10|10|10|10|10|
@@ -61,33 +61,33 @@ function resetMap() {
 		10|03 03 03 03 03 03 03 03 03 03 03 03 03 03 03 03 03 03 03 03 03 10|
 		10|10|10|10|10|10|10|10|10|10|10|10|10|10|10|10|10|10|10|10|10|10|10|
 	`);
-  Hack.maps.map1 = map1;
+	Hack.maps.map1 = map1;
 
-  Hack.changeMap('map1'); // map1 をロード
+	Hack.changeMap('map1'); // map1 をロード
 
-  for (var i = 3; i <= 13; i += 2) {
-    for (var j = 3; j < 100; j += 2) {
-      putTresureBox(i, j);
-    }
-  }
+	for (var i = 3; i <= 13; i += 2) {
+		for (var j = 3; j < 100; j += 2) {
+			putTresureBox(i, j);
+		}
+	}
 
-  /*+ モンスター アイテム せっち システム */
+	/*+ モンスター アイテム せっち システム */
 }
 
 function putTresureBox(x, y) {
-  const itemBox = new RPGObject();
-  itemBox.mod(('▼ スキン', _tたからばこ));
-  itemBox.locate(x, y, 'map1');
-  itemBox.onこうげきされた = () => {
-    delete itemBox.onこうげきされた;
-    itemBox.mod(('▼ スキン', _tたからばこひらいた));
-    Hack.score += mTresureBoxScore;
-  };
+	const itemBox = new RPGObject();
+	itemBox.mod(('▼ スキン', _tたからばこ));
+	itemBox.locate(x, y, 'map1');
+	itemBox.onこうげきされた = () => {
+		delete itemBox.onこうげきされた;
+		itemBox.mod(('▼ スキン', _tたからばこひらいた));
+		Hack.score += mTresureBoxScore;
+	};
 }
 
 Hack.onreset = function() {
-  resetMap();
-  player.locate(1, 1); // はじめの位置
+	resetMap();
+	player.locate(1, 1); // はじめの位置
 };
 
 export default gameFunc;
