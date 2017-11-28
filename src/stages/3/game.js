d226be9e10c3c7328d273c5f5c6b2faf1961b3aa
teramotodaiki <i101323@gmail.com>
2017-11-28 19:02:25 +0900

    バリアをアニメーション / リファクタリング

--- src/stages/3/game.js
+++ src/stages/3/game.js
@@ -7,9 +7,6 @@ import * as sequence from 'sequence';
 var mDragonScore = 200;
 var mDragonHp = 1;
 var mOrbScore = 40;
-var flagGem1 = false;
-var flagGem2 = false;
-var flagGem3 = false;
 async function gameFunc() {
 
 	resetMap();
@@ -114,6 +111,19 @@ function resetMap() {
 		player.locate(7, 8); // はじめの位置
 	});
 
+	// オーブを壊した後呼ぶ関数
+	let orbNum = 3;
+	const destroyOrb = () => {
+		orbNum--; // オーブを 1 へらす
+		Hack.score += mOrbScore; // 得点を増やす		
+		if (orbNum === 0) {
+			// ドラゴンを倒せる状態にする
+			itemDragon.hp = mDragonHp;
+			// バリアを消す
+			itemBarrier.visible = false;
+		}
+	};
+
 	const itemDragon = new RPGObject();
 	itemDragon.mod(('▼ スキン', _dドラゴン));
 	// itemDragon.hp = 10;
@@ -126,75 +136,44 @@ function resetMap() {
 		Hack.score += mDragonScore;
 	});
 	itemDragon.on(('▼ イベント', 'こうげきされた'), () => {
-		// 全てのフラグが立っている。
-		if (flagGem1 && flagGem2 && flagGem3) {
-
-		} 
-		// そうでない＝攻撃を受け付けない
-		else {
-			// itemGem1.color = 'red';
-			// itemGem2.color = 'red';
-			// itemGem3.color = 'red';
-
-			itemBarrier.tl.clear().show().delay(10).fadeTo(0.7, 30).then(() => {
-				// itemGem1.color = 'brown';
-				// itemGem2.color = 'brown';
-				// itemGem3.color = 'brown';
-			});
-			// itemBarrier.tl.fadeIn(0).delay(10).fadeOut(20);
+		if (orbNum > 0) {
+		// オーブが一つでも残っているなら攻撃を受け付けない
+			itemBarrier.tl.clear().show().delay(10).fadeTo(0.7, 30);
 		}
 	});
 
 	const itemGem1 = new RPGObject();
-	flagGem1 = false;	
 	itemGem1.mod(('▼ スキン', Hack.assets.orangeOrb));
 	itemGem1.hp = 1;
 	itemGem1.locate(4, 3, 'map1');
 	itemGem1.tl.moveBy(0, 96, 60).moveBy(0, -96, 60).loop();
-	itemGem1.on(('▼ イベント', 'たおれたとき'), () => {
-		flagGem1 = true;
-		if (flagGem2 && flagGem3) {
-			itemDragon.hp = mDragonHp;
-			itemBarrier.visible = false;
-		}
-		Hack.score += mOrbScore;
-	});
+	itemGem1.on(('▼ イベント', 'たおれたとき'), destroyOrb);
 
 
 	const itemGem2 = new RPGObject();
-	flagGem2 = false;	
 	itemGem2.mod(('▼ スキン', Hack.assets.orangeOrb));
 	itemGem2.hp = 1;
 	itemGem2.locate(10, 6, 'map1');
 	itemGem2.tl.moveBy(0, -96, 60).moveBy(0, 96, 60).loop();
-	itemGem2.on(('▼ イベント', 'たおれたとき'), () => {
-		flagGem2 = true;
-		if (flagGem1 && flagGem3) {
-			itemDragon.hp = mDragonHp;
-			itemBarrier.visible = false;
-		}
-		Hack.score += mOrbScore;
-	});
+	itemGem2.on(('▼ イベント', 'たおれたとき'), destroyOrb);
 
 
 	const itemGem3 = new RPGObject();
-	flagGem3 = false;
 	itemGem3.mod(('▼ スキン', Hack.assets.orangeOrb));
 	itemGem3.hp = 1;
 	itemGem3.locate(8, 2, 'map1');
 	itemGem3.tl.moveBy(-64, 0, 60).moveBy(64, 0, 60).loop();
-	itemGem3.on(('▼ イベント', 'たおれたとき'), () => {
-		flagGem3 = true;
-		if (flagGem1 && flagGem2) {
-			itemDragon.hp = mDragonHp;
-			itemBarrier.visible = false;
-		}
-		Hack.score += mOrbScore;
-	});
+	itemGem3.on(('▼ イベント', 'たおれたとき'), destroyOrb);
+
 	const itemBarrier = new Sprite(128, 128);
 	itemBarrier.image = game.assets['resources/barrier'];
 	itemBarrier.moveTo(173, 80);
 	itemBarrier.opacity = 0.7;
+	itemBarrier.tl.scaleTo(1.1, 20).scaleTo(0.9, 20).loop();
+	itemBarrier.on('enterframe', () => {
+		const scale = 0.4 + 0.2 * orbNum + 0.05 * Math.sin(itemBarrier.age / 10);
+		itemBarrier.scaleX = itemBarrier.scaleY = scale;
+	});
 	Hack.defaultParentNode.addChild(itemBarrier);
 
 	/*+ モンスター アイテム せっち システム */
